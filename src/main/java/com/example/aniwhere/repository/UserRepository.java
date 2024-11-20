c14c26f7da5e2fdd3f50e56c549ba2cf32b1096f
jwj <dnwls16071@naver.com>
2024-11-20 17:59:45 +0900

    [refactor] 리포지토리 코드 리팩토링

--- src/main/java/com/example/aniwhere/repository/UserRepository.java
+++ src/main/java/com/example/aniwhere/repository/UserRepository.java
@@ -1,12 +1,10 @@
-package com.example.aniwhere.infrastructure.persistence;
+package com.example.aniwhere.repository;
 
 import com.example.aniwhere.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 }
