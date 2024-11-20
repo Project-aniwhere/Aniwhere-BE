4a33e803e4adf78a3c506423b1982d88a8b196e0
jwj <dnwls16071@naver.com>
2024-11-20 18:08:10 +0900

    [refactor] 토큰 DTO를 record로 변환 및 타입 정리

--- src/main/java/com/example/aniwhere/domain/token/dto/JwtToken.java
+++ src/main/java/com/example/aniwhere/domain/token/dto/JwtToken.java
@@ -1,12 +1,5 @@
 package com.example.aniwhere.domain.token.dto;
 
-import lombok.*;
+public record JwtToken(String accessToken, String refreshToken) {
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-public class JwtToken {
-	private String accessToken;
-	private String refreshToken;
 }
