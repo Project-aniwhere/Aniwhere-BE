517ceba06a86b34ec86bc06b5e75b15f2b03ab4a
jwj <dnwls16071@naver.com>
2024-12-09 16:14:56 +0900

    [chore] Swagger v3 마이그레이션: API 경로 수정 및 401 인증 예외 처리

--- src/main/java/com/example/aniwhere/application/config/SwaggerConfig.java
+++ src/main/java/com/example/aniwhere/application/config/SwaggerConfig.java
@@ -1,37 +1,25 @@
 package com.example.aniwhere.application.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Aniwhere Project",
-				description = "Aniwhere Backend REST API 문서",
-				version = "v1"
-		),
-		security = @SecurityRequirement(name = "Bearer Authentication")
-)
-@SecurityScheme(
-		name = "Bearer Authentication",
-		type = SecuritySchemeType.HTTP,
-		bearerFormat = "JWT",
-		scheme = "bearer",
-		in = SecuritySchemeIn.HEADER
-)
 public class SwaggerConfig {
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	public OpenAPI openAPI() {
 		return new OpenAPI()
-				.components(new Components());
+				.info(new Info()
+						.title("AniWhere API Documentation")
+						.version("1.0.0")
+						.description("AniWhere API 명세서"))
+				.servers(List.of(
+						new Server().url("/")
+				));
 	}
 }
