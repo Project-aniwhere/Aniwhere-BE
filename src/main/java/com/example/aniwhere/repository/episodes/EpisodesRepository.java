5de5c230f7223efe37a5541343efa681a5e184da
jwj <dnwls16071@naver.com>
2024-12-10 00:43:13 +0900

    [feat] 에피소드 리포지토리 추가

---
+++ src/main/java/com/example/aniwhere/repository/episodes/EpisodesRepository.java
@@ -0,0 +1,7 @@
+package com.example.aniwhere.repository.episodes;
+
+import com.example.aniwhere.domain.episodes.Episodes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EpisodesRepository extends JpaRepository<Episodes, Long> {
+}
