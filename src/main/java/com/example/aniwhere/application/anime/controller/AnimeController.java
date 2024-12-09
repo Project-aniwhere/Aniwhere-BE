69c4048546c2f3ad7117cb104dbd6253927542eb
kimuche <angela_id@naver.com>
2024-12-09 21:51:32 +0900

    [docs] swagger 추가

--- src/main/java/com/example/aniwhere/application/anime/controller/AnimeController.java
+++ src/main/java/com/example/aniwhere/application/anime/controller/AnimeController.java
@@ -5,9 +5,19 @@ import com.example.aniwhere.domain.anime.dto.AnimeDTO;
 import com.example.aniwhere.domain.anime.dto.AnimeDTO.*;
 import com.example.aniwhere.application.anime.service.AnimeService;
 import com.example.aniwhere.global.error.ErrorCode;
+import com.example.aniwhere.global.error.ErrorResponse;
 import com.example.aniwhere.global.error.exception.InvalidInputException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -48,6 +58,14 @@ public class AnimeController {
         return ResponseEntity.ok(animeResponse);
     }
 
+    /**
+     *
+     * @return WeekdayAnimeDTO
+     */
+    @Operation(
+            summary = "요일별 애니메이션 조회",
+            description = "요일별 애니메이션을 월요일(1) ~ 일요일(7) 순으로 조회합니다."
+    )
     @GetMapping("/anime/weekday")
     public ResponseEntity<Map<Integer, List<WeekdayAnimeDTO>>> getWeekdayAnimeList(){
         Map<Integer, List<WeekdayAnimeDTO>> animeResponse = animeService.getAnimeWeekdayList();
