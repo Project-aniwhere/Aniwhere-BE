9197d1dee09e39eda005e5c6537a72f844115e24
jwj <dnwls16071@naver.com>
2024-12-15 16:23:21 +0900

    [fix] 코드 리뷰 피드백 반영(운채님) - DB 작업 리포지토리 측으로 이관

--- src/main/java/com/example/aniwhere/repository/episodes/EpisodesRepository.java
+++ src/main/java/com/example/aniwhere/repository/episodes/EpisodesRepository.java
@@ -3,5 +3,6 @@ package com.example.aniwhere.repository.episodes;
 import com.example.aniwhere.domain.episodes.Episodes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EpisodesRepository extends JpaRepository<Episodes, Long> {
+public interface EpisodesRepository extends JpaRepository<Episodes, Long>, EpisodesRepositoryCustom {
+
 }
---
+++ src/main/java/com/example/aniwhere/repository/episodesReview/EpisodesReviewRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.example.aniwhere.repository.episodesReview;
+
+import com.example.aniwhere.domain.episodeReviews.dto.EpisodeReviewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EpisodesReviewRepositoryCustom {
+	Page<EpisodeReviewResponse> getEpisodeReviews(Long episodeId, Pageable pageable);
+}
