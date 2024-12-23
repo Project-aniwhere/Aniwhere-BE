69d35959e1e557313a06b3bde9b692ed917ced1f
jwj <dnwls16071@naver.com>
2024-12-23 20:51:33 +0900

    [feat] 에피소드 리뷰 커스텀 예외 코드 추가

--- src/main/java/com/example/aniwhere/global/error/ErrorCode.java
+++ src/main/java/com/example/aniwhere/global/error/ErrorCode.java
@@ -39,7 +39,9 @@ public enum ErrorCode {
 	SERVICE_UNAVAILABLE(503, "E002", "서비스를 사용할 수 없습니다."),
 
 	// episodes
-	NOT_FOUND_EPISODE(404, "EP001", "해당 에피소드를 찾을 수 없습니다.");
+	NOT_FOUND_EPISODE(404, "EP001", "해당 에피소드를 찾을 수 없습니다."),
+	ALREADY_EXIST_EPISODE_REVIEW(400, "EP002", "리뷰는 한 번만 작성할 수 있습니다."),
+	NOT_FOUND_EPISODE_REVIEW(404, "EP003", "작성한 에피소드 리뷰가 없습니다.");
 
 	private final int status;
 	private final String code;
