def5c985c634cce53ac5e6982bd91d58327e8816
kwh1208 <kwh871005@gmail.com>
2025-01-05 09:47:40 +0900

    [feat] 분기별 애니 삭제(/api/anime/quarter)

--- src/main/java/com/example/aniwhere/controller/anime/controller/AnimeController.java
+++ src/main/java/com/example/aniwhere/controller/anime/controller/AnimeController.java
@@ -20,25 +20,7 @@ import java.util.Map;
 public class AnimeController {
 
     private final AnimeService animeService;
-    /**
-     *
-     * @param year 방영년도
-     * @param quarter 방영분기
-     * @return
-     */
-    @GetMapping("/anime/quarter")
-    public ResponseEntity<Map<String, List<QuarterAnimeResponseDTO>>> getAnimeByQuarter(@RequestParam int year, @RequestParam int quarter) {
-        if (year < 1990 ||year > 2030) {
-            throw new InvalidInputException("연도는 1990년 이상, 2030년 이하여야 합니다.", ErrorCode.INVALID_INPUT_VALUE);
-        }
-        if (quarter < 1 || quarter > 4) {
-            throw new InvalidInputException("분기는 1 이상, 4 이하여야 합니다.", ErrorCode.INVALID_INPUT_VALUE);
-        }
 
-        Map<String, List<QuarterAnimeResponseDTO>> animeGroupedByWeekday = animeService.getAnimeByYearAndQuarter(year, quarter);
-
-        return ResponseEntity.ok(animeGroupedByWeekday);
-    }
 
     @GetMapping("/anime/{id}")
     public ResponseEntity<AnimeResponseDTO> getAnimeById(@PathVariable int id) {
