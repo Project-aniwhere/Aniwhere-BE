6daac4b2cf6921526d5b077dc24172b72943055e
kwh1208 <kwh871005@gmail.com>
2025-01-12 18:48:42 +0900

    [feat] 추천 알고리즘 성능을 위해 캐시 구현 추가

---
+++ src/main/java/com/example/aniwhere/application/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.example.aniwhere.application.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
