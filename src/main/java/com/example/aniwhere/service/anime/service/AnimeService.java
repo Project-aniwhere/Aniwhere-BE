3c79a472a9ea2409e9c2532460c8537ab66d4769
kwh1208 <117095342+kwh1208@users.noreply.github.com>
2025-02-14 19:22:57 +0900

    Update AnimeService.java

--- src/main/java/com/example/aniwhere/service/anime/service/AnimeService.java
+++ src/main/java/com/example/aniwhere/service/anime/service/AnimeService.java
@@ -110,5 +110,25 @@ public class AnimeService {
 
         return animeRepository.findAllGroupedByWeekday(year, quarter);
     }
+
+
+    public Map<String, List<QuarterAnimeResponseDTO>> getAnimeByYearAndQuarter(int year, int quarter) {
+        List<Anime> animes = animeRepository.findByYearAndQuarter(year, quarter);
+
+
+        List<String> weekdays = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일");
+
+        // 요일별로 response
+        Map<String, List<QuarterAnimeResponseDTO>> groupedAnimes = weekdays.stream()
+                .collect(Collectors.toMap(day -> day, day -> new ArrayList<>(), (a, b) -> a, LinkedHashMap::new));
+
+        // 애니메이션 요일별로
+        animes.stream()
+                .filter(anime -> weekdays.contains(anime.getWeekday())) // 요일이 없는 경우 고려
+                .map(this::convertToDTO)
+                .forEach(anime -> groupedAnimes.get(anime.getWeekday()).add(anime));
+
+        return groupedAnimes;
+    }
 }
 
