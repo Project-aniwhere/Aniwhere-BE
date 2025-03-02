3cf42c14a2b5d44b34f9912ead43b1925379fd8d
kimuche <angela_id@naver.com>
2025-03-02 19:59:28 +0900

    [feat] 애니메이션 검색 기능 구현

--- src/main/java/com/example/aniwhere/repository/anime/repository/AnimeCustomRepository.java
+++ src/main/java/com/example/aniwhere/repository/anime/repository/AnimeCustomRepository.java
@@ -1,7 +1,12 @@
 package com.example.aniwhere.repository.anime.repository;
 
+import com.example.aniwhere.application.config.page.PageResponse;
 import com.example.aniwhere.domain.anime.Anime;
 import com.example.aniwhere.domain.anime.dto.AnimeDTO;
+import com.example.aniwhere.domain.anime.dto.AnimeSearchResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +16,9 @@ import java.util.Map;
 public interface AnimeCustomRepository {
 
     List<AnimeDTO.AnimeGroupedByWeekdayDTO> findAllGroupedByWeekday(Integer year, Integer quarter);
+
+//    PageResponse<AnimeSearchResponseDto> findByFilters(List<String> categories, List<Integer> quarters, String title, List<String> statuses, PageRequest pageRequest);
+
+    PageResponse<AnimeSearchResponseDto> findByFilters(
+            List<String> categories, List<Integer> quarters, String title, List<String> statuses, Integer year, PageRequest pageRequest);
 }
--- src/main/java/com/example/aniwhere/service/tag/TagService.java
+++ src/main/java/com/example/aniwhere/service/tag/TagService.java
@@ -1,10 +1,17 @@
 package com.example.aniwhere.service.tag;
 
+import com.example.aniwhere.application.config.page.PageResponse;
+import com.example.aniwhere.domain.anime.Anime;
+import com.example.aniwhere.domain.anime.dto.AnimeSearchResponseDto;
 import com.example.aniwhere.domain.category.Category;
 import com.example.aniwhere.domain.category.dto.CategoryDTO;
+import com.example.aniwhere.repository.anime.repository.AnimeRepository;
 import com.example.aniwhere.repository.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +24,29 @@ import java.util.stream.Collectors;
 public class TagService {
 
     private final CategoryRepository categoryRepository;
+    private final AnimeRepository animeRepository;
 
+
+    /**
+     * 카테고리 목록을 조회
+     */
     @Transactional(readOnly = true)
     public List<CategoryDTO.CategoryResponseDTO> getCategoriesList() {
         return categoryRepository.findAll().stream()
                 .map(CategoryDTO.CategoryResponseDTO::of)
                 .collect(Collectors.toList());
     }
+
+    /**
+     * 태그 기반 Anime 검색
+     */
+    @Transactional(readOnly = true)
+    public PageResponse<AnimeSearchResponseDto> searchAnimeByTag(
+            List<String> categories, List<Integer> quarters, String title, List<String> statuses, Integer year, PageRequest pageRequest) {
+
+        log.info("QueryDSL 태그 검색 요청 - 카테고리: {}, 분기: {}, 제목: {}, 상태: {}", categories, quarters, title, statuses);
+
+        return animeRepository.findByFilters(categories, quarters, title, statuses, year, pageRequest);
+    }
+
 }
