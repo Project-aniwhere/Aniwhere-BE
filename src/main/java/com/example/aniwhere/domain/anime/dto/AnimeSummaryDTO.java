07eb524a989f0781269d707db4aa64643b66cdb4
kwh1208 <kwh871005@gmail.com>
2025-04-13 15:02:30 +0900

    [feat] 추천리스트 반환값 카테고리, 방영시작날짜 추가

--- src/main/java/com/example/aniwhere/domain/anime/dto/AnimeSummaryDTO.java
+++ src/main/java/com/example/aniwhere/domain/anime/dto/AnimeSummaryDTO.java
@@ -2,6 +2,11 @@ package com.example.aniwhere.domain.anime.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.example.aniwhere.domain.category.dto.CategoryDTO.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,6 +19,8 @@ public class AnimeSummaryDTO {
     private String poster;
     private String studio;
     private int episodes;
+    private LocalDate releaseDate;
     private double averageRating;
     private String latestReview;
+    private List<CategoryResponseDTO> categories;
 }
